f2409ed2bf2663ba7f1bfa6c7a92a26806a77db7
freshcodeAlice <freshjs.ofte@gmail.com>
2023-10-05 09:53:26 +0300

    browser modals

--- index.js
+++ index.js
@@ -1,47 +1,14 @@
 /*
-Логічне І  (&&)
+Модальні вікна
 
-Результат - перший операнд, який був false, або останній операнд, якщо всі були true
-*/
+alert(message) - Повідомляє користувача про message
+prompt(message) - Запитує message і повертає результат (ЯКИЙ ЗАВЖДИ Є РЯДКОМ)
+confirm(message) - Повідомляє message і користувач натискає Ok або Cancel (відповідно резльтутатом виклику буде true або false)
 
-let age = 30;
-
-// потрібно дізнатись, чи вік користувача в діапазоні від 20 до 40 років
-
-/*
-age >= 20  &&  age <= 40
-true            true            === true
-false           true            === false
-true            false           === false
-false           false           === false
-
-Логічне І (AND) виконує всі умови, якщо всі вони - true, то результатом буде true
-Якщо хоч хтось з операндів - false, то результатом буде false
-
-Якщо операнд буде не Boolean, то він автоматично конвертується для порівняння в Boolean
-*/
-
-/*
-Логічне АБО (||)
-
-Результат - повертає перший true-операнд, або останній false, якщо всі були false
-*/
-
-
-false || null || undefined
-
-/*
-age >= 20  ||  age <= 40
-true            true        === true
-false           true        === true
-true            false       === true
-false           false       === false
 
 */
 
 
-let userName = 0;
+let result = prompt('Скільки вам років?');
 
-let consoleMessage = 'Привіт, ' + (userName || 'Аноним');
-
-console.log(consoleMessage);
+alert(Number(result));
