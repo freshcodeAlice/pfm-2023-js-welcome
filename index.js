e293081bc3f76ebd9c7dfa4e8fb70d1f2d1339cf
freshcodeAlice <freshjs.ofte@gmail.com>
2023-10-17 11:05:02 +0300

    User randomiser

--- index.js
+++ index.js
@@ -1,142 +1,56 @@
 /* Методи масивів */
 
-// forEach(функція) - приймає функцію і виконує її на кожному елементі масиву
-// результатом роботи forEach завжди є undefined
-// Не змінює елементи масиву
+/*
+name
+lastName
+email
+age
+gender
 
-const array = [4, 5, 6, 7, 8];
+*/
 
-array.forEach(callback)
+const USERNAMES = ['John', 'Josh', 'Jane', 'Rick', 'Keyli', 'Li', 'Bill', 'Mark', 'Sten'];
 
 
-function callback(currentValue, index, array) {
-    console.log(`${currentValue} --- ${index} --- ${array}`);
+function User(name, lastName, email, age, gender){
+    this.name = name;
+    this.lastName = lastName;
+    this.email = email;
+    this.age = age;
+    this.gender = gender
+}
+
+
+function generateOneUser(){
+    const name = USERNAMES[getRandom(0, USERNAMES.length)];
+     const user = new User(name, 'Doe', `mail${getRandom(0, 100)}@mail.com}`, getRandom(10, 100), (Math.random() > 0.5) ? 'male' : 'female');
+     return user;
+    }
+
+function getRandom(min, max) {  // генерація рандомного числа в заданому діаізоні
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+function getUsersArray(dimension) {
+    const arr = new Array(dimension);
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = generateOneUser();
+    }
+    return arr;
 }
 
 
 
+/*
+Умовний (тернарний) оператор
 
-function myOwnForEach(array, fn){
-    for (let i = 0; i < array.length; i++) {
-      fn(array[i], i, array);        
-    }
+if(умова) {
+    блок коду, якщо умова - true
+} else {
+    блок коду, якщо умова - false
 }
 
-
-const users = [{
-    name: 'John',
-    lastName: 'Doe',
-    email: '123@mail', 
-    age: 15
-}, {
-    name: 'Josh',
-    lastName: 'Foe',
-    email: '321@mail',
-    age: 20
-}, {
-    name: 'Jack',
-    lastName: 'Crow',
-    email: '456@mail',
-    age: 18,
-},{
-    name: 'Jane',
-    lastName: 'Snow',
-    email: '654@mail',
-     age: 13
-}];
-
-/*
-users.forEach(function(currentUser){
-    currentUser.age = 20;
-})
-*/
-
-/*
-Вивести мейли всіх юзерів на консоль
-*/
-
-
-users.forEach(function(currentUser) {
-    console.log(currentUser.email);
-})
-
-
-/*  map() */
-
-
-/*  map(функція) - приймає функцію, виконує її на кожному елементі початкового масиву і в якості результату повертає новий масив з результатами роботи коллбеку */
-
-
-const emails = users.map(function(currentValue, index, array) {
-    return currentValue.email
-}); // ['123@mail', '321@mail', '456@mail', '654@mail']
-
-
-
-
-/* filter() - фільтрує елементи масива на співпадіння умові */
-
-
-// filter(функція) - створює новий масив, який складається з елементів початкового, які "пройшли перевірку" -
-// функція, яку передали методу, повертає true - елемент пройшов
-// функція повертає false - елемент не пройшов
-
-
-const filtered = users.filter(function(currentValue, index, array) {
-    return currentValue.age > 18
-});
-
-
-/*
-Створити масив [4, 5, 6, 7, 8, 10]
-Вільтрувати його так, щоби в результаті отримати масив з [4, 6, 8, 10]
+result = (умова) ? (результат, якщо умова - true) : (результат, якщо умова - false)
 
 */
-
-const res = [4, 5, 6, 7, 8, 10].filter(function(curNum) {
-    return curNum % 2 === 0;
-});
-
-
-
- /*  Sort */
- 
-
- const arr1 = [7, 5, 23, 1, 2, 4];
-
- /// Сортує масив
-
- // sort(функція) - приймає функцію і сортує елементи масива відповідно до результатів роботи цієї функції
- // мутує масив (змінює початковий)
-
- function comparator(a, b) {   // порівнююча функція
-    if (a > b) {  // якщо наступний елемент - більше ніж попередній - він має залишитись на місці
-        return 1;
-    } else if( a < b) { // якщо наступний менше - вони поміняються місцями
-        return -1;
-    } else {
-        return 0;
-    }
- }
-
-
- arr1.sort(comparator)
-
-
-/// Задача: відсортувати юзерів за віком
-
-users.sort(function (u1, u2) {
-    return u1.age - u2.age
-});
-
-
-/*   reduce */
-
-// Метод reduce зводить всі елементи масива до якогось одного результату
-
-const arr5 = [3, 4, 5, 6, 7, 8, 9];
-
-// Знайти суму всіх елементів масиву
-const result = arr5.reduce(function (accumulator, currentValue) {
-    return accumulator + currentValue
-}, 0);
