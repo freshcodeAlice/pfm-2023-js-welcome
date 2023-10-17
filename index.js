ef791270957a0421f675c355c89fbfea800a7b46
freshcodeAlice <freshjs.ofte@gmail.com>
2023-10-17 09:31:33 +0300

    HOF

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-/* Методи масива */
-
-/*
-join
-
-*/
-const arr = [2, 3, 4, 5];
+/* Функції як аргументи інших функцій */
 
 
-function myJoin(array, separator = ',') {
-    let str = '';
-    for (let i = 0; i < array.length - 1; i++) {
-        str += array[i]+separator   
-    }
-    return str + array[array.length - 1];
-    // результатом роботи має бути рядок, який є результатом склеювання всіх елементів масива
+
+function multyTwo(argum) {
+    return argum*2;
 }
+
+function square(argum) {
+    return argum**2;
+}
+
+const fn2 = multyTwo; // передача посилання на функцію
+
+/* Функція вищого порядку (High order function - HOF) */
+function getConsoleExp(number, func) {   // функція - аргумент іншої функції
+    const res = func(number);  // func - callback - функція зворотнього виклику
+    console.log(res);
+}
+
+
+
+
+getConsoleExp(4, multyTwo);
+getConsoleExp(3, square);
