a8053409b5859578b64702766cb864241203159b
freshcodeAlice <freshjs.ofte@gmail.com>
2023-10-24 11:14:17 +0300

    Math

--- index.js
+++ index.js
@@ -4,75 +4,28 @@ Date
 Math
 Errors
 
-Date - не тип даних!
-Це окремий об'єкт, який представляє певну точку часу у вигляді таймштемпу
 */
 
-const date1 = new Date();
+// Math
 
-// копіювання дати: date -> timestamp (кількість мілісекунд) -> створити новий об'єкт з заданим часом
-const timestamp = date1.getTime();
-const copyDate = new Date(timestamp); // новий об'єкт з тою самою зафіксованою точкою часу
+Math.random() // рандомні значення від 0 до 1
 
+// Округлення
+Math.ceil() // округлення вверх, до найбільшого цілого
 
-/* Задача: скільки днів залишилось до кінця місяця? */
+Math.floor() // округлення вниз, до найменшого цілого
 
-
-
-const months = [31,28,31,30,31,30,31,31,30,31,30,31];
-
-const date = new Date();
-const howMuchDaysLeft = months[date.getMonth()] - date.getDate();
-
-console.log(howMuchDaysLeft);
+Math.round() // до найближчого цілого: 0.1 - 0.49 -> 0,  0.5-0.99 -> 1 
 
 
-/* Задача: вивести назву поточного дня тижня */
-
-const week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-
-console.log(week[date.getDay()-1]);
+Math.trunc() // відсікає дрібну частину
 
 
-/*
-Вивести години, хвилини і секунди поточної дати у вигляді:
+Math.min() // найменше з запронованих аргументів
 
-Зараз XX годин XX хвилин XX секунд
-*/
+Math.max() // найбільше з запронованих аргументів
 
-console.log(`Зараз ${date.getHours()} годин ${date.getMinutes()} хвилин ${date.getSeconds()} секунд`);
+//Math.pow(base, power) === base ** power
 
 
-/*
-Написати функцію, яка запитує у користувача двома prompt-ами день і місяць його народження і виводить alert-ом, скільки днів лишилось до його дня народження.
-
-Якщо в поточному році день народження вже був, то вивести кількість днів до наступного
-
-1. Спитати користувача, коли він народився
-2. Створити новий об'єкт з датою з цими даними
-3. Порівняти з поточною датою, якщо день народження вже пройшов - додати рік і порахувати, скільки лишилось.
-
-
-milliseconds -> days: 100*60*60*24
-*/
-
-
-function getDaysLeftToBirthday(){
-    const userBirthDay = Number(prompt('Введіть день свого народження'));
-    const userBirthMonth = Number(prompt('Введіть місяць свого народження'));
-
-    const today = new Date(); // витягнути рік
-    let userbDate = new Date(today.getFullYear(), userBirthMonth - 1, userBirthDay);
-   
-    if(userbDate < today) {    //// якщо в цьому році вже дн був - додати рік
-        userbDate = new Date(userbDate.setFullYear(today.getFullYear() + 1));
-    }
-
-    const millisecondsInDay = 1000*60*60*24;
-
-    const res = (userbDate - today) / millisecondsInDay;
-  alert(`До дн залишилось ${res.toFixed()} днів`)
-
-}
-
-getDaysLeftToBirthday()
+Math.sqrt() // квадратний корінь з числа
