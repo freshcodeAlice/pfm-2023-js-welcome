40f3d939e740b73d1347452f8eb3d827bb7dbd53
freshcodeAlice <freshjs.ofte@gmail.com>
2023-10-26 10:13:25 +0300

    JSDoc (+tasks resolve)

--- index.js
+++ index.js
@@ -1,70 +1,3 @@
-/* Стандартні вбудовані об'єкти
-
-Date
-Math
-Errors
-
-*/
-
-
-// Errors
-
-/*
-Помилка - це об'єкт
-*/
-
-
-/*
-Типи помилок:
-
-
- TypeError - некоректна робота з типами даних
- ReferenceError - виклик неіснуючої змінної/доступ до неіснуючого об'єкту
- SyntaxError - помилка написання коду
- RangeError - вихід за діапазон значень
-
- URIError - помилка з URI
- InternalError - помилка всередині рушія JS. Нестандартна, може ніколи не зустрітись
- EvalError - помилка при використанні функції eval(). Не використовуйте eval
-
-*/
-
-
-
-/// Таска: написати функцію, яка знаходить добуток двох чисел
-
-function multy(num1, num2) {
-    if (typeof num1 !== 'number' || typeof num2 !== 'number'){
-        // викинути помилку
-        throw new TypeError('Both arguments must be numbers');  // throw викидає об'єкт помилки і зупиняє подальшу роботу коду
-    }
-    return num1*num2;
-}
-
-
-console.log('Код працював, працював, і викликав функцію')
-
-
-
-
-
-// Передбачення роботи з помилками - це обробка помилок
-
-
-try {
-     // блок коду, який може викидати помилки
-
-     const result = multy('aa', 'aaa');
-    console.log(result);  // не відпрацює
-    /// цей блок виконується до першої помилки
-} catch(error){
-    // в цьому блоці коду, який виконається ТІЛЬКИ ЯКЩО в блоці try сталася помилка - об'єкт помилки лежить в error
-    console.log(error);
-}
-
-
-console.log('Код працює далі')
-
 
 /*
 Написати функцію, яка приймає два числа - start і end, повертає масив, наповнений числами від числа start до числа end. Включно з крайніми числами
@@ -76,6 +9,15 @@ console.log('Код працює далі')
 
 2, 5 -> [2, 3, 4, 5]
 */
+
+/**
+ * 
+ * @param {Number} start - Start of number sequence
+ * @param {Number} end - End of number sequence
+ * @throws {TypeError} - If arguments is not a number
+ * @throws {RangeError} - If start < end
+ * @returns {Array} - array with numbers between start and end
+ */
 
 function getRange(start, end) {
     if (typeof start !== 'number' || typeof end !== 'number') {
@@ -98,4 +40,104 @@ try {
     getRange('d', 1);
 } catch(er) {
     console.log(er.message) // повідомлення помилки
+}
+
+
+/* JSDocs - документація коду
+
+1. Пишемо перед функцією.
+*/
+
+//getRange(undefined, undefined)
+
+
+//  Таска: написати функцію, яка приймає два числа і повертає добуток цих чисел. У випадку, якщо аргументи - не числа - викинути помилку. І описати функцію в JSDoc
+
+/**
+ * 
+ * @param {Number} num1 - First number
+ * @param {Number} num2 - Second number
+ * @throws {TypeError} - If one of arguments is not a number
+ * @returns {Number} - Multiply of two numbers
+ */
+
+function multy(num1, num2) {
+    if (typeof num1 !== 'number' || typeof num2 !== 'number'){
+        throw new TypeError('Both arguments must numbers');
+    }
+    return num1*num2;
+}
+
+
+try {
+    multy(2, 'h');
+} catch(error){
+    console.log('Multy error:')
+    console.log(error);
+}
+
+
+/*
+Коли помилка викидається (throw) - вона передається від функції до функції, поки не викинеться в глобальний код і не зупинить інтерпретацію скрипта
+
+*/
+
+
+function getErr() {
+    /// щось робить
+    function newErr() {
+        /// функція теж щось робить
+        anotherFunc()
+        function anotherFunc() {
+            ///
+            throw new Error('Some error');
+        }
+    }
+
+
+    newErr()
+}
+
+try {
+    getErr();
+} catch(error) {
+    console.log('Ooops! Something goes wrong. Did you broke something?')
+}
+
+
+/*
+Таска: написати функцію, яка приймає масив з числами і число. Повертає новий масив, в якому кожен елемент = елемент з початкового масиву, помножений на прийняте в якості другого аргументу число
+
+Якщо перший аргумент - не масив - видати помилку,
+якщо другий аргумент - не число - видати помилку, якщо він = 0 - повернути пустий масив
+
+Описати функцію в JSDoc
+
+*/
+
+/**
+ * 
+ * @param {Array} arr 
+ * @param {Number} num 
+ * @throws {TypeError} - if arguments is not fit
+ * @returns {Array(Number)} - array with multyplied elements
+ */
+
+function multyArr(arr, num) {
+    if (!Array.isArray(arr)){
+        throw new TypeError('First argument must be an array');
+    }
+    if (typeof num !== 'number') {
+        throw new TypeError('Second argument must be a number')
+    }
+    if (num === 0) {
+        return [];
+    }
+    return arr.map((el) => el*num);
+}
+
+try {
+    console.log(multyArr([2, 3, 1, 2, 2], 's'));
+} catch(error) {
+    console.log('Function error')
 }
