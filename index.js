6c84268954fa5487a6d68892461c6a14db2df448
freshcodeAlice <freshjs.ofte@gmail.com>
2023-10-27 09:40:26 +0300

    call stack

--- index.js
+++ index.js
@@ -1,161 +1,28 @@
-/* Замикання */
-
-/*
-Замикання - це комбінація функції та її лексичного оточення (області видимості)
-*/
-
-// let variable = 'John';
-
-function printName() {
-     let variable = 'Rick';
-    console.log(variable);
-
-    return function (){
-        console.log(`${variable} say Hello`); /// Замикання - це те, що ось ця внутрішня функція ЗАПАМ'ЯТАЛА значення variable
-    }
-
-
+function fun1(){
+    debugger;
+    console.log('first function');
+    fun2(); //stop
+    console.log('first continue')
 }
 
-    const result = printName(); // printName відпрацювала і вмерла
-
-    result();
-
-
-    function makeCounter(){
-        let i = 0;
-        let j = 5;
-        return function() {
- //           debugger;
-            return i++;
-        }
-
-        /// на цьому функція makeCounter всьо
-    }
-
-    const firstCounter = makeCounter();
-
-    const secondCounter = makeCounter();
-
-    function fullName(lastName){
-        return function(firstName) {  // lastName буде запам'ятовуватись із
-            return `${firstName} ${lastName}`;
-        }
-    }
-
-    const family = fullName('Doe');
-    // family('John')
-
-    const liFamily = fullName('Li');
-    liFamily('John') // John Li
-    liFamily('Rick li') // rick li
-
-
-    function getURL(protocol){
-        return function (domain) {
-            return `${protocol}://${domain}`; 
-        }
-    }
-
-    const https = getURL('https'); ///  запам'ятала протокол https
-  //  https('google.com');
-  // https('wikipedia.org'); 
-
-  const ftp = getURL('ftp'); // повертається посилання на внутрішню функцію, яка запам'ятала ftp
-  // ftp('google.com') ---> ftp://google.com
-
-  const ws = getURL('ws') // websocket
-
-
-  const urlCreater = protocol => domain => `${protocol}://${domain}`;
-
-
-  /*
-Створити функцію createAdder, яка приймає параметр m і повертає функцію, що приймає параметр n і повертає суму m + n
-
-// createAdder(10) -> inner(20) // 10+20 
-                    -> inner(30) // 10+30
-   */
-
-//  function createAdder(m) {
-//     return function(n){
-//         return m+n
-//     }
-//  }
-
-const createAdder = m => n => n+m;
-
- const inner = createAdder(10);
-const inner2 = createAdder(50);
-
-
-/*  Каррування (currying) - це процес перетворення функції, яка приймає n аргументів на n функцій, які приймають 1 або більше аргументів
-Зниження арності функції
-арність (розмірність) - кількість аргументів функції
-
-*/
-
-// https://google.com/path?key1=value2
-
-function URL(protocol, domain, path, queryParams) {
-    return `${protocol}://${domain}/${path}?${queryParams}`
+function fun2(){
+    console.log('second function');
+    fun3(); // stop
 }
 
-// function fullURL(protocol) {
-//     return function(domain) {
-//         return function (path) {
-//             return function(queryParams='') {
-//                 return `${protocol}://${domain}/${path}${queryParams ? `?${queryParams}` : ''}`
-//             }
-//         }
-//     }
-// }
-
-
-const fullURL = protocol => domain => path => (queryParams = '') => `${protocol}://${domain}/${path}${queryParams ? `?${queryParams}` : ''}`;
-
-const URLhttp = fullURL('http');
-const URLdom = URLhttp('mysait.com'); // http, 'mysait'
-
-const URLpath = URLdom('index');
-//const URLquery = URLpath('key1=valu1');
-
-const doneURL = fullURL('http')('masait.com')('page.html')('key=value');
-
-
-///// об'єкт і замикання
-
-function makeAPICounter() {
-    let count = 0;
-    return {
-        increment() {
-            return ++count;
-        },
-        decrement() {
-            return --count;
-        }
-    }
+function fun3(){
+    console.log('third function')
 }
 
-const objCounter = makeAPICounter(); //  повертається об'єкт, який має два методи, що зберігають count в замиканні
 
+fun1()
 
 
 /*
-Написати функцію, що повертає об'єкт з методами push(), pop(), які додають в кінець і відповідно видаляють з кінця елементи масива, що зберігається у замиканні
+Стек - структура даних, яка реалізує принцип LIFO (last in - first out)
+Найпростіший приклад - банка Pringles
+
+У браузері виклики функцій складаються у Call Stack
 
 
 */
-
-
-function makeArray(){
-    const arr = [];
-    return {
-        push: function(v) {
-            return arr.push(v);
-        },
-        pop: function(){
-            return arr.pop()
-        }
-    }
-}
