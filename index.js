40e6ae185efec018e758d8920ba0fdfda8e8af4a
freshcodeAlice <freshjs.ofte@gmail.com>
2023-10-27 10:21:39 +0300

    recursive function

--- index.js
+++ index.js
@@ -1,28 +1,51 @@
-function fun1(){
-    debugger;
-    console.log('first function');
-    fun2(); //stop
-    console.log('first continue')
-}
-
-function fun2(){
-    console.log('second function');
-    fun3(); // stop
-}
-
-function fun3(){
-    console.log('third function')
-}
-
-
-fun1()
+/* Рекурсія */
 
 
 /*
-Стек - структура даних, яка реалізує принцип LIFO (last in - first out)
-Найпростіший приклад - банка Pringles
+Рекурсія - функція викликає саму себе (з іншими аргументами)
 
-У браузері виклики функцій складаються у Call Stack
 
+База рекурсії - умова, за якої наступного виклику функції не станеться, а замість цього повернеться обчислений результат
+*/
+
+//debugger;
+function factorial(n){
+    if (n === 1) {  // база рекурсії
+        return 1;
+    }
+  
+    return n*factorial(n-1) /// рекурсивний випадок
+}
+
+
+/// 5! = 1*2*3*4*5
+
+
+//factorial(5);
+
+
+/*
+Написати рекурвну функцію, яка зводить число у заданий ступінь
+
+pow(base, power)
+5**3 = 5*5*5 = 125
+5**1 = 5
 
 */
+
+/*
+function pow(base, power){
+    if (power === 1) {
+        return base;
+    }
+    return base * pow(base, power-1)
+}
+*/
+
+const pow = (base, power) => (power === 1) ? base : base * pow(base, power-1)
+
+
+
+
+const res = pow(5, 4);
+console.log(res);
