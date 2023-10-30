4197c31a6b71c9424727623e5324a71e8e1dcbae
freshcodeAlice <freshjs.ofte@gmail.com>
2023-10-30 11:36:29 +0200

    getters and setters

--- index.js
+++ index.js
@@ -62,48 +62,6 @@ const user2 = new User('John', 'Doe', 20, 'joe@gmail.com');
 
 
 
-/*
-Створити клас Worker
-- name
-- rate
-- days
-
-Метод getSalary(), який розраховує зп за формулою: rate*days
-
-
-*/
-
-
-class Worker{
-    constructor(name, rate, days) {
-        this.name = name;
-        if (rate < 0) {
-            throw new RangeError('Rate must be great that 0');
-        }
-        this.rate = rate;
-        if (days < 0) {
-            this.days = 0;
-        } else {
-            this.days = days;
-        }
-
-    }
-
-    getSalary() {
-        return this.rate * this.days
-    }
-}
-
-
-const wrkr = new Worker('John', 5, 20);
-
-/*
-ставка не може бути < 0, якщо передали -ставку, викинути помилку
-кількість відпрацьованих днів не може бути < 0, то не викидати, замість цього this.days = 0
-
-
-*/
-
 
 
 /*
@@ -198,3 +156,120 @@ const fr2 = new Friend('Rick', 20, fr);
 Як дізнатись, що об'єкт є екземпляром певного класу
 
 */
+
+
+
+
+
+/*
+Створити клас Worker
+- name
+- rate
+- days
+
+Метод getSalary(), який розраховує зп за формулою: rate*days
+
+
+*/
+
+
+class Worker{
+    constructor(name, rate, days) {
+        this.name = name;
+       //  this._rate = rate;
+        // Замість вручну встановлення _rate - скористаємось сеттером:
+        this.rate = rate;
+
+
+        // this._days = days; -- Замість цього користуємось сеттером:
+        this.days = days; 
+    }
+
+
+    // get - віддавати, set - встановлювати
+
+    /*
+    setRate(value) {
+        if (value < 0) {
+            throw new RangeError('Rate must be great than 0');
+        }
+        this._rate = value;
+        return this._rate;
+    }
+
+    getRate(){
+        return this._rate;
+    }
+    */
+
+    get rate(){
+        return this._rate;
+    }
+
+    /*
+    Використання геттера:
+    wrkr.tariff
+
+    */
+
+    set rate(value){  // 1 argument must be
+        if (value < 0) {
+            throw new RangeError('Rate must be great than 0');
+        }
+        this._rate = value;
+    }
+
+    /*
+    wrkr.tariff = 10;
+
+    */
+
+    get days() {
+        return this._days;
+    }
+
+    
+    set days(value){
+        if (value < 0) {
+            this._days = 0
+        } else {
+            this._days = value;
+        }
+    }
+    
+
+    getSalary() {
+        return this.rate * this.days
+    }
+}
+
+
+const wrkr = new Worker('John', 5, 20);
+
+/*
+ставка не може бути < 0, якщо передали -ставку, викинути помилку
+кількість відпрацьованих днів не може бути < 0, то не викидати, замість цього this.days = 0
+
+
+*/
+
+
+// після створення екземпляру зміни полів не валідуються
+
+/// угода розробників: приватні поля мають перед ім'ям _
+
+// приватні поля (підтримка вже є, але з'явилась нещодавно, неоновлені/старі браузери можуть не підтримувати цю можливість) - ім'я поля починається з #
+// цей механізм реалізований у мові
+
+
+
+/*
+Інкапсуляція - приховання певної складнощі або даних від зовнішнього впливу
+
+Якщо має бути можливість зміни - має бути наданий інтерфейс (відповідний метод, який зможе це зробити)
+
+
+*/
+
+
+// Таска - приховати поле days і зробити геттери та сеттери для нього
